c22973bcc8e850dbbbf74f8a16c65180dc9fbbf1
[brandon hammond] <[BHammond@student.fullsail.edu]>
2023-02-11 10:38:35 -0600

    Grader functional in node. No input validation yet."

--- grader.js
+++ grader.js
@@ -14,3 +14,45 @@
 // 5. 102.1
 // Hint: Think about the order in which if/elseif/else evaluates your conditions. Do you test for an "A" condition first and work your way down, or test for "F" and work your way up?
 // Show errors in command line if there are any.
+
+class Grader{
+    constructor() {
+        const rl = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        rl.question("What is your name? ",(name)=>{
+            rl.question("What is the name of the assignment? ",(assignment)=>{
+                rl.question("What is your number grade? ",(grade)=>{
+                    grade = parseFloat(grade);
+                    this.displayGrade(name, assignment, grade);
+                    rl.close();
+                })
+                
+            })
+        })
+    }
+
+    displayGrade(n,a,g) {
+        let letter;
+        if (g >= 90) {
+            letter = "A";
+        }
+        else if (g >= 80) {
+            letter = "B";
+        }
+        else if (g >= 70) {
+            letter = "C";
+        }
+        else if (g >= 60) {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+        console.log(n + " you made a " + letter + " on " + a);
+    }
+}
+
+let grades = new Grader()
